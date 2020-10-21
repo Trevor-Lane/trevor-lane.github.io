40cf74d89b9978572111c0e7757c129352666fee
Trevor-Lane <trevorlane2001@gmail.com>
2020-10-21 08:49:57 -0700

    Too many changes

---
+++ pprograms/sketch.js
@@ -0,0 +1,23 @@
+var x =0;
+var y =200;
+var diameter=50;
+var r =218;
+var g= 160;
+var b=221;
+
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+function draw() {
+  fill(200, 200, 200);
+  ellipse(circle.x, circle.y, circle.diamator,circle)
+    
+    
+  x=x+1;
+  
+    
+  
+  
+}
